bc03619906855ceee10df7363f67bd66843d24a9
suhailthajudeen <suhail@zoiteckh.com>
2024-06-11 20:50:29 +0530

    Fix:Revoked Origin URL - checksum issue

--- Package.swift
+++ Package.swift
@@ -69,7 +69,7 @@ let package = Package(
         // ),
         .binaryTarget(name: "sqflite",
                           url: "https://github.com/PatientOrderSets/thinkresearch_messenger_ios_swift_sdk/releases/download/v1.0.0/sqflite.xcframework.zip",
-                          checksum: "0607ada3aa1aee1ce315c2f7ac6473462a49e109890288def4aa61e9ef37c66f"),
+                          checksum: "79d78e8d693ff70f0c26471188f808c34a1d577153bcce9969da61aa1a3425da"),
         .binaryTarget(
             name: "url_launcher_ios",
             path: "Frameworks/Release/url_launcher_ios.xcframework"
